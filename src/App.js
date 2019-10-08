1e90c02f137b7c03954a07d1acf509ebf9db96a0
Lucas Barrios <Lucas@MacBook-Air-de-Lucas.local>
2019-10-08 14:31:08 -0300

    Se eliminaron los archivos header, footer y body y se agregó el archivo BarraBusqueda. También se realizarin algunos cambios en el archivo App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import SearchAppBar from './BarraBusqueda';
+
+function Home(){
+  return <h2>Home</h2>;
+}
+function Tasks(){
+  return <h2>tasks</h2>;
+}
+
+function Navegador(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+        <nav>
+         <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/tasks">Tasks</Link>
+            </li>
+          </ul>
+        </nav>
+      
+         {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+       <Switch>
+          <Route path="/tasks">
+          <Tasks/>
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
+
+    
+  );
+}
+
+
+
+function App (){
+  return (
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <header className = "App-header">   
+           <SearchAppBar/>
+          </header>
+        </Grid>
+        <Grid item xs = {8}> 
+          <div>
+            <nav>
+              <Navegador/>
+            </nav>
+          </div>
+        </Grid>
+  
+      </Grid>
+      
     </div>
   );
 }
 
-export default App;
+
+
+  export default App;
